6af512c57306b03fea18a90d329b53a75c42b71a
Mingjian Wen <wenxx151@gmail.com>
2021-09-15 15:19:40 -0700

    Create nequip energy model

---
+++ eigenn/model_factory/nequip_energy_model.py
@@ -0,0 +1,95 @@
+from collections import OrderedDict
+from typing import Any, Dict
+
+from nequip.data import AtomicDataDict
+from nequip.nn import AtomwiseLinear, AtomwiseReduce, ConvNetLayer
+from nequip.nn.embedding import (
+    OneHotAtomEncoding,
+    RadialBasisEdgeEncoding,
+    SphericalHarmonicEdgeAttrs,
+)
+
+from eigenn.model_factory.utils import create_sequential_module
+
+
+def energy_model(hparams: Dict[str, Any]):
+
+    num_layers = hparams.pop("num_layers", 3)
+
+    # ===== embedding layers =====
+    layers = {
+        # -- Encode --
+        "one_hot": (
+            OneHotAtomEncoding,
+            {
+                "num_species": 3,
+                "irreps_in": {"node_features": hparams["species_embedding_irreps_out"]},
+            },
+        ),
+        "spharm_edges": (
+            SphericalHarmonicEdgeAttrs,
+            {"irreps_edge_sh": hparams["irreps_edge_sh"]},
+        ),
+        "radial_basis": (
+            RadialBasisEdgeEncoding,
+            {
+                "basis_kwargs": {"r_max": hparams["r_max"]},
+                "cutoff_kwargs": {"r_max": hparams["r_max"]},
+            },
+        ),
+        # -- Embed features --
+        "chemical_embedding": (AtomwiseLinear, {}),
+    }
+
+    # ===== convnet layers =====
+    # insertion preserves order
+    for i in range(num_layers):
+        layers[f"layer{i}_convnet"] = (
+            ConvNetLayer,
+            {
+                "feature_irreps_hidden": hparams["feature_irreps_hidden"],
+            },
+        )
+
+    # .update also maintains insertion order
+    layers.update(
+        {
+            # TODO: the next linear throws out all L > 0, don't create them in the last layer of convnet
+            # -- output block --
+            "conv_to_output_hidden": (AtomwiseLinear, {}),
+            "output_hidden_to_scalar": (
+                AtomwiseLinear,
+                dict(irreps_out="1x0e", out_field=AtomicDataDict.PER_ATOM_ENERGY_KEY),
+            ),
+        }
+    )
+
+    layers["total_energy_sum"] = (
+        AtomwiseReduce,
+        dict(
+            reduce="sum",
+            field=AtomicDataDict.PER_ATOM_ENERGY_KEY,
+            out_field=AtomicDataDict.TOTAL_ENERGY_KEY,
+        ),
+    )
+
+    model = create_sequential_module(
+        modules=OrderedDict(layers), use_kwargs_irreps_in=True
+    )
+
+    return model
+
+
+if __name__ == "__main__":
+    from eigenn.log import set_logger
+
+    set_logger(level="DEBUG")
+
+    params = {
+        "species_embedding_irreps_out": "16x0e",
+        "feature_irreps_hidden": "32x0o + 32x0e + 16x1o + 16x1e",
+        "irreps_edge_sh": "0e + 1o",
+        "num_layers": 3,
+        "r_max": 4,
+    }
+    energy_model(params)
