a444e2df735925ca4e842f61ac875317c13ba996
Mingjian Wen <wenxx151@gmail.com>
2021-09-15 15:19:15 -0700

    Add utils functions to create Sequential module

---
+++ eigenn/model_factory/utils.py
@@ -0,0 +1,87 @@
+from collections import OrderedDict
+from typing import Dict, Optional, Tuple
+
+from e3nn.o3 import Irreps
+from loguru import logger
+
+from eigenn.layer.irreps import ModuleIrreps, Sequential
+
+
+def create_sequential_module(
+    modules: "OrderedDict[str, Tuple[ModuleIrreps, Dict]]",
+    irreps_in: Optional[Dict[str, Irreps]] = None,
+    use_kwargs_irreps_in: bool = False,
+) -> Sequential:
+    """
+    Create a sequential module from a sequence of modules.
+
+    The main purpose of this function is to automatically determine the irreps for
+    consecutive modules, i.e. using the output irreps of module i as the input irreps
+    of module i+1.
+
+    Args:
+        modules: modules to create the sequential module. It should be look like
+            {name: (ModuleClass, kwargs)}. ``kwargs`` should be a dictionary and it will
+            be used to instantiate the class via ``ModuleClass(**kwargs)``.
+            Note, the output irreps of the previous module will be added to kwargs with
+            key ``irreps_in``.
+        irreps_in: input irreps for the first module.
+        use_kwargs_irreps_in: whether use the irreps_in if it is provided as kwargs for a
+            module. If `True`, will combine the irreps_out of module i-1 and the
+            irreps_in in kwargs of module i (this with higher priority) as the irreps_in
+            for module i.
+
+    Returns:
+        a sequential module
+    """
+
+    module_instances = []
+    for name, (cls_type, kwargs) in modules.items():
+
+        # TODO, enable the below when no nequip stuff is not used
+        # if not issubclass(cls_type, ModuleIrreps):
+        #     raise TypeError(
+        #         "Expect all modules be a subclass of `MolduleIrreps`. "
+        #         f"`{cls_name}` is not for layer with name `{name}`."
+        #     )
+
+        if len(module_instances) == 0:
+            ir = irreps_in
+        else:
+            ir = module_instances[-1].irreps_out
+
+        if "irreps_in" in kwargs:
+            if use_kwargs_irreps_in:
+                if ir is not None:
+                    # order matters: irreps_in in kwargs takes higher priority
+                    ir.update(kwargs["irreps_in"])
+                else:
+                    ir = kwargs["irreps_in"]
+            else:
+                raise ValueError(
+                    f"Trying to automatically determine irreps_in for module {name} "
+                    f"But it is provided as kwargs. Set `use_kwargs_irrpes_in=True` to "
+                    f"force it."
+                )
+
+        kwargs = kwargs.copy()
+        kwargs["irreps_in"] = ir
+
+        cls_name = cls_type.__name__
+        try:
+            m = cls_type(**kwargs)
+        except Exception:
+            raise RuntimeError(
+                f"Failed instantiate module `{cls_name}` with kwargs: `{kwargs}`"
+            )
+        logger.info(f"Instantiate module `{cls_name}` with kwargs: `{kwargs}`")
+        logger.debug(
+            f"Module `{cls_name}` has irreps_in = `{m.irreps_in}`, and has "
+            f"irreps_out = `{m.irreps_out}`"
+        )
+
+        module_instances.append(m)
+
+    modules_dict = OrderedDict(zip(modules.keys(), module_instances))
+
+    return Sequential(modules_dict)
