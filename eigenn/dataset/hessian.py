ea3901376e6a41512b9bb8ef33a4c0d3cf175b6b
mjwen <wenxx151@gmail.com>
2022-01-28 15:02:00 -0800

    Fix to use new data collate such that hessian_layout is corrected indexed

--- eigenn/dataset/hessian.py
+++ eigenn/dataset/hessian.py
@@ -1,12 +1,16 @@
 import itertools
 import warnings
+from collections.abc import Mapping, Sequence
 from pathlib import Path
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 import ase.data
 import ase.io
 import numpy as np
 import torch
+import torch.utils.data
+from torch.utils.data.dataloader import default_collate
+from torch_geometric.data import Batch, Data, Dataset, HeteroData
 
 from eigenn.data.data import Molecule
 from eigenn.data.datamodule import BaseDataModule
@@ -22,7 +26,7 @@ class HessianDataset(InMemoryDataset):
          root:
     """
 
-    def __init__(self, filename: str, root: Union[str, Path] = ".", reuse=True):
+    def __init__(self, filename: str, root: Union[str, Path] = ".", reuse=False):
         super().__init__(
             filenames=[filename], root=root, processed_dirname=f"processed", reuse=reuse
         )
@@ -73,7 +77,7 @@ class HessianDataset(InMemoryDataset):
                     x=None,
                     y={
                         "hessian": hessian,
-                        "hessian_layout": hessian_layout,
+                        "hessian_layout_raw": hessian_layout,
                         "hessian_natoms": hessian_natoms,
                     },
                     atomic_numbers=conf.get_atomic_numbers(),
@@ -153,6 +157,108 @@ class HessianDataMoldule(BaseDataModule):
             "allowed_species": tuple(atomic_numbers),
             "average_num_neighbors": average_num_neighbors,
         }
+
+    def train_dataloader(self):
+        return DataLoader(dataset=self.train_data, **self.loader_kwargs)
+
+    def val_dataloader(self):
+        return DataLoader(dataset=self.val_data, **self.loader_kwargs)
+
+    def test_dataloader(self):
+        return DataLoader(dataset=self.test_data, **self.loader_kwargs)
+
+
+#
+# Collater and DataLoader copied from PyG.
+# We need to use `hessian_layout` to select node features. In a sense, it is similar
+# to how edge_index should be treated when batching: i.e. the index of the next graph
+# should be added by the total number of edges in the previous graph. However,
+# since we provide it as an attribute in y, this will not be done by the default
+# collate function. Here, we modify it to deal with this.
+#
+class Collater(object):
+    def __init__(self, follow_batch, exclude_keys):
+        self.follow_batch = follow_batch
+        self.exclude_keys = exclude_keys
+
+    def collate(self, batch):
+        elem = batch[0]
+        if isinstance(elem, Data) or isinstance(elem, HeteroData):
+
+            # @@@ NOTE, add Data.y.hessian_layout
+            i = 0
+            for d in batch:
+                # should use different name to not overwrite hessian_layout_raw
+                d["y"]["hessian_layout"] = d["y"]["hessian_layout_raw"] + i
+                i += d.num_nodes
+            # @@@
+
+            return Batch.from_data_list(batch, self.follow_batch, self.exclude_keys)
+        elif isinstance(elem, torch.Tensor):
+            return default_collate(batch)
+        elif isinstance(elem, float):
+            return torch.tensor(batch, dtype=torch.float)
+        elif isinstance(elem, int):
+            return torch.tensor(batch)
+        elif isinstance(elem, str):
+            return batch
+        elif isinstance(elem, Mapping):
+            return {key: self.collate([d[key] for d in batch]) for key in elem}
+        elif isinstance(elem, tuple) and hasattr(elem, "_fields"):
+            return type(elem)(*(self.collate(s) for s in zip(*batch)))
+        elif isinstance(elem, Sequence) and not isinstance(elem, str):
+            return [self.collate(s) for s in zip(*batch)]
+
+        raise TypeError("DataLoader found invalid type: {}".format(type(elem)))
+
+    def __call__(self, batch):
+        return self.collate(batch)
+
+
+class DataLoader(torch.utils.data.DataLoader):
+    r"""A data loader which merges data objects from a
+    :class:`torch_geometric.data.Dataset` to a mini-batch.
+    Data objects can be either of type :class:`~torch_geometric.data.Data` or
+    :class:`~torch_geometric.data.HeteroData`.
+
+    Args:
+        dataset (Dataset): The dataset from which to load the data.
+        batch_size (int, optional): How many samples per batch to load.
+            (default: :obj:`1`)
+        shuffle (bool, optional): If set to :obj:`True`, the data will be
+            reshuffled at every epoch. (default: :obj:`False`)
+        follow_batch (List[str], optional): Creates assignment batch
+            vectors for each key in the list. (default: :obj:`[]`)
+        exclude_keys (List[str], optional): Will exclude each key in the
+            list. (default: :obj:`[]`)
+        **kwargs (optional): Additional arguments of
+            :class:`torch.utils.data.DataLoader`.
+    """
+
+    def __init__(
+        self,
+        dataset: Union[Dataset, List[Data], List[HeteroData]],
+        batch_size: int = 1,
+        shuffle: bool = False,
+        follow_batch: List[str] = [],
+        exclude_keys: List[str] = [],
+        **kwargs,
+    ):
+
+        if "collate_fn" in kwargs:
+            del kwargs["collate_fn"]
+
+        # Save for PyTorch Lightning...
+        self.follow_batch = follow_batch
+        self.exclude_keys = exclude_keys
+
+        super().__init__(
+            dataset,
+            batch_size,
+            shuffle,
+            collate_fn=Collater(follow_batch, exclude_keys),
+            **kwargs,
+        )
 
 
 if __name__ == "__main__":
