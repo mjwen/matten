65173356f46668590635127bebbdd2b7062e10ff
mjwen <wenxx151@gmail.com>
2022-01-27 21:43:38 -0800

    Add IrrepsToHessian and test

--- eigenn/nn/readout.py
+++ eigenn/nn/readout.py
@@ -2,9 +2,10 @@ from typing import Dict, Optional
 
 import torch
 from e3nn.io import CartesianTensor
-from e3nn.o3 import Irreps
+from e3nn.o3 import FullyConnectedTensorProduct, Irreps
 
 from eigenn.data.irreps import DataKey, ModuleIrreps
+from eigenn.nn.utils import tp_path_exists
 
 
 class IrrepsToCartesianTensor(ModuleIrreps, torch.nn.Module):
@@ -38,10 +39,12 @@ class IrrepsToCartesianTensor(ModuleIrreps, torch.nn.Module):
 
         self.ct = CartesianTensor(formula=formula)
 
-    def forward(self, data: DataKey.Type) -> DataKey.Type:
-        # shallow copy so input `data` is not modified
-        data = data.copy()
+        assert self.irreps_in[self.field] == self.ct, (
+            f"input irreps of {self.field} is {self.irreps_in[self.field]}, not equal "
+            f"to the irreps of the target irreps {self.ct}"
+        )
 
+    def forward(self, data: DataKey.Type) -> DataKey.Type:
         value = data[self.field]
         cartesian_tensor = self.ct.to_cartesian(value)
         data[self.out_field] = cartesian_tensor
@@ -54,3 +57,64 @@ class IrrepsToCartesianTensor(ModuleIrreps, torch.nn.Module):
             f"  field: {self.field}, out_field: {self.out_field}, formula: {self.formula}\n"
             ")"
         )
+
+
+class IrrepsToHessian(ModuleIrreps, torch.nn.Module):
+    def __init__(
+        self,
+        irreps_in: Dict[str, Irreps],
+        field: str = DataKey.NODE_FEATURES,
+        out_field: Optional[str] = None,
+    ):
+        """
+        Convert irreps tensor to 3N by 3N Hessian matrix of a configuration.
+
+        Each 3x3 sub matrix in the rows 3i~3i+3 and columns 3j~3j+3 of the Hessian
+        matrix is constructed from the node features of atom i, and atom j (by
+        mapping the tensor products of the node features to a general 3x3 matrix).
+
+        Args:
+            irreps_in:
+            field:
+            out_field:
+        """
+        super().__init__()
+
+        self.field = field
+        self.out_field = field if out_field is None else out_field
+
+        # NOTE, should not add output to irreps_out, since it is a cartesian tensor,
+        # no longer an irreps
+        self.init_irreps(irreps_in, required_keys_irreps_in=[field])
+
+        # for each sub 3x3 matrix of the hessian, it is a general 2D matrix
+        self.ct = CartesianTensor(formula="ij=ij")  # i.e. 0e+1e+2e
+
+        # ensure tp between two irreps_in can product self.ct
+        for mul, ir in self.ct:
+            assert tp_path_exists(
+                self.irreps_in[self.field], self.irreps_in[self.field], ir
+            ), f"Product between two irreps_in cannot product {ir}"
+
+        self.tp = FullyConnectedTensorProduct(
+            self.irreps_in[self.field], self.irreps_in[self.field], self.ct
+        )
+
+    def forward(self, data: DataKey.Type) -> DataKey.Type:
+        node_feats = data[self.field]
+
+        layout = data["hessian_layout"]
+        row_index = layout[:, 0]
+        column_index = layout[:, 1]
+
+        # convert node feats to node pair feats, with of shape
+        # shape of x: (num_index, 9), where num_index = N1^2 +N2^2 + ..., in which
+        # N1, N2... are the number of atoms in the batched molecules
+        x = self.tp(node_feats[row_index], node_feats[column_index])
+
+        # convert irreps tensor to cartesian tensor
+        cartesian_tensor = self.ct.to_cartesian(x)  # (num_index, 3, 3)
+
+        data[self.out_field] = cartesian_tensor
+
+        return data
---
+++ tests/nn/test_readout.py
@@ -0,0 +1,42 @@
+import torch
+
+from eigenn.data.irreps import DataKey
+from eigenn.nn.readout import IrrepsToCartesianTensor, IrrepsToHessian
+
+
+def test_irreps_to_cartesian():
+
+    i2c = IrrepsToCartesianTensor(
+        irreps_in={DataKey.NODE_FEATURES: "0e + 2e"},
+        formula="ij=ji",
+        out_field="my_out",
+    )
+
+    t = torch.arange(9).to(torch.float).view(3, 3)
+    y = i2c.ct.from_cartesian(t)
+
+    data = {DataKey.NODE_FEATURES: y}
+    out = i2c(data)
+    z = out["my_out"]
+
+    assert torch.allclose(z, (t + t.T) / 2, atol=1e-5)
+
+
+def test_irreps_to_hessian():
+
+    i2h = IrrepsToHessian(
+        irreps_in={DataKey.NODE_FEATURES: "0e + 1e + 2e"},
+        out_field="my_out",
+    )
+
+    natoms = 2
+    t = torch.arange(natoms * 9).to(torch.float).view(natoms, 3, 3)
+    y = i2h.ct.from_cartesian(t)
+    assert y.shape == (natoms, 9)  # 9 corresponds to '0e+1e+2e'
+
+    layout = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1]])
+    data = {DataKey.NODE_FEATURES: y, "hessian_layout": layout}
+
+    out = i2h(data)
+    z = out["my_out"]
+    assert z.shape == (4, 3, 3)
