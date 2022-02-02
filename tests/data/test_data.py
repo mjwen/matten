ac045ad524c9dacd7cc9bdbe80d770f3c6ded735
mjwen <wenxx151@gmail.com>
2022-02-02 12:50:35 -0800

    Add tests for edges using pymatgen BabelNN

--- tests/data/test_data.py
+++ tests/data/test_data.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
+import ase.io
 import numpy as np
 from torch_geometric.data import Batch
 
-from eigenn.data.data import DataPoint
+from eigenn.data.data import DataPoint, pmg_mol_graph
+
+TEST_FILE_DIR = Path(__file__).resolve().parents[1].joinpath("test_files")
 
 
 def test_AtomicData():
@@ -19,3 +24,40 @@ def test_AtomicData():
     print(data)
     print("\n\n\n")
     print(batch)
+
+
+def test_pmg_mol_graph():
+
+    file = TEST_FILE_DIR.joinpath("mol.xyz")
+    atoms = ase.io.read(file, format="extxyz")
+    pos = atoms.positions
+    species = atoms.symbols
+
+    edges, num_neigh = pmg_mol_graph(pos, species)
+
+    ref_edges = np.asarray(
+        [
+            [0, 6],
+            [0, 1],
+            [0, 4],
+            [0, 5],
+            [1, 2],
+            [1, 0],
+            [2, 7],
+            [2, 3],
+            [2, 1],
+            [3, 8],
+            [3, 4],
+            [3, 2],
+            [4, 0],
+            [4, 3],
+            [5, 0],
+            [6, 0],
+            [7, 2],
+            [8, 3],
+        ]
+    ).T
+    ref_num_neigh = [4, 2, 3, 3, 2, 1, 1, 1, 1]
+
+    assert np.array_equal(edges, ref_edges)
+    assert num_neigh == ref_num_neigh
