72bbe9c213cd0d3f57eeb1f9a2d03f01d3b76294
mjwen <wenxx151@gmail.com>
2022-04-04 18:44:14 -0700

    Add equivariance test for hessian model

--- tests/model/test_tfh_hessian.py
+++ tests/model/test_tfh_hessian.py
@@ -2,10 +2,15 @@
 Test the hessian model to ensure:
 - the diagonal blocks are symmetric
 - two off-diagonal blocks (i,j) and (j,i) are transpose of each other
+- test - basically using the formula: $f(Qx) = Qf(x)$, where $f$ is the model
+  and $x$ are the coords in input molecules.
 """
+import tempfile
 from pathlib import Path
 
 import torch
+from ase.io.extxyz import read_extxyz, write_extxyz
+from e3nn import o3
 from e3nn.io import CartesianTensor
 
 from eigenn.dataset.hessian import DataLoader, HessianDataset
@@ -62,8 +67,8 @@ def test_hessian_model():
             graphs = batch.tensor_property_to_dict()
             out = model(graphs)
 
-            diag = out["hessian_diag"]
-            off_diag = out["hessian_off_diag"]
+            diag = out["hessian_ii_block"]
+            off_diag = out["hessian_ij_block"]
 
             layout = batch.y["hessian_off_diag_layout"]
 
@@ -73,7 +78,7 @@ def test_hessian_model():
     convert = CartesianTensor("ij=ji")
     for x in diag:
         x = convert.to_cartesian(x)
-        assert torch.allclose(x, x.T)
+        assert torch.allclose(x, x.T, rtol=1e-2)
 
     # off diag
     convert = CartesianTensor("ij=ij")
@@ -87,5 +92,61 @@ def test_hessian_model():
         kk = layout.index([j, i])
         y = off_diag[kk]
         y = convert.to_cartesian(y)
+        assert torch.allclose(x, y.T, rtol=1e-2)
 
-        assert torch.allclose(x, y.T)
+
+def test_hesssian_equivariance():
+
+    model = get_model()
+
+    filename = TESTFILE_DIR.joinpath("test_files", "hessian_one.xyz")
+    loader = load_dataset(filename, root="/tmp")
+
+    # filename = "ani1_CHO_0-1000_hessian_one.xyz"
+    # root = "/Users/mjwen/Documents/Dataset/xiaowei_hessian"
+    #
+    # loader = load_dataset(filename=filename, root=root)
+
+    #
+    # generate a temp file with rotated atoms, and get it data loader
+    #
+    # original mol
+    with open(filename, "r") as f:
+        atoms = read_extxyz(f)
+        atoms = next(atoms)
+
+    # transformation matrix
+    torch.manual_seed(35)
+    Q = o3.rand_matrix()
+
+    # update positions
+    new_positions = atoms.positions @ Q.numpy().T
+    atoms.positions = new_positions
+
+    # write it out
+    filename2 = "hessian_one-rotated.xyz"
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with open(Path(tmpdir).joinpath(filename2), "w") as f:
+            write_extxyz(f, atoms)
+        loader_rotated = load_dataset(filename=filename2, root=tmpdir)
+
+    def get_result(model, loader):
+        model.eval()
+
+        with torch.no_grad():
+            for batch in loader:
+                graphs = batch.tensor_property_to_dict()
+                out = model(graphs)
+
+                # return out['hessian_ii_block']
+                return out["hessian_ij_block"]
+
+    pred = get_result(model, loader)
+    pred_rotated = get_result(model, loader_rotated)
+
+    convert = CartesianTensor("ij=ij")
+    pred_cart = convert.to_cartesian(pred)
+    pred_rotated_cart = convert.to_cartesian(pred_rotated)
+
+    for h, h_rotated in zip(pred_cart, pred_rotated_cart):
+        assert torch.allclose(Q @ h @ Q.T, h_rotated, rtol=1e-2)
