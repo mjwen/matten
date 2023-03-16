ab3cc18dd4ad1a463c6e687fbcf8250d05925eac
mjwen <wenxx151@gmail.com>
2023-03-16 14:10:40 -0500

    Remove outdated data

--- tests/nn/test_convolution.py
+++ tests/nn/test_convolution.py
@@ -1,24 +1,7 @@
 from e3nn.o3 import Irreps
 
 from matten.data.irreps import DataKey
-from matten.nn.point_conv import PointConv
 from matten.nn.transformer_conv import TransformerConv
-
-
-def test_Conv():
-    for ConvClass in [PointConv]:
-
-        conv = ConvClass(
-            irreps_in={
-                DataKey.NODE_FEATURES: Irreps("4x1e+2x0e"),
-                DataKey.NODE_ATTRS: Irreps("4x1e+2x0e"),
-                DataKey.EDGE_ATTRS: Irreps("4x1e+2x0e"),
-                DataKey.EDGE_EMBEDDING: Irreps("4x0e"),
-            },
-            irreps_out={
-                DataKey.NODE_FEATURES: Irreps("8x1e+4x0e"),
-            },
-        )
 
 
 def test_Transformer():
