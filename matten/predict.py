5e9571a595a37aa56ddacd7ccd40f520e88dc81b
mjwen <wenxx151@gmail.com>
2023-06-22 22:00:56 -0500

    Testing predict script

--- matten/predict.py
+++ matten/predict.py
@@ -7,29 +7,68 @@ from pymatgen.core.structure import Structure
 from torch_geometric.loader import DataLoader
 
 from matten.dataset.structure_scalar_tensor import TensorDatasetPrediction
+from matten.log import set_logger
 from matten.model_factory.tfn_scalar_tensor import ScalarTensorModel
 from matten.utils import CartesianTensorWrapper, yaml_load
 
 
-def get_pretrained_model_info(identifier: str = "20230601"):
+def get_pretrained_model_dir(identifier: str = "20230601"):
+    return Path(__file__).parent.parent / "pretrained" / identifier
+
+
+def get_pretrained_model(
+    identifier: str = "20230601", checkpoint: str = "model_final.ckpt"
+):
     """Get the pretrained model and its training config."""
 
-    directory = Path(__file__).parent.parent.parent / "pretrained" / identifier
-
+    directory = get_pretrained_model_dir(identifier)
     model = ScalarTensorModel.load_from_checkpoint(
-        checkpoint_path=directory.joinpath("model_final.ckpt").as_posix(),
+        checkpoint_path=directory.joinpath(checkpoint).as_posix(),
         map_location="cpu",
     )
-
-    config = yaml_load(directory / "config_final.yaml")
-
-    return model, config
+    return model
 
 
-def get_data_loader(structure: list[Structure], config: dict, batch_size: int = 200):
+def get_pretrained_config(
+    identifier: str = "20230601", config_filename: str = "config_final.yaml"
+):
+    directory = get_pretrained_model_dir(identifier)
+    config = yaml_load(directory / config_filename)
+
+    return config
+
+
+def get_data_loader(structures: list[Structure], config: dict, batch_size: int = 200):
+    # config contains info for dataset and data loader, we only use the dataset part,
+    # and adjust some parameters
+    config = config["data"].copy()
+    for k in [
+        "loader_kwargs",
+        "root",
+        "trainset_filename",
+        "valset_filename",
+        "testset_filename",
+        "compute_dataset_statistics",
+    ]:
+        config.pop(k)
+
+    r_cut = config.pop("r_cut")
+    config["dataset_statistics_fn"] = None
+
     dataset = TensorDatasetPrediction(
-        filename=None, r_cut=config["rcut"], structures=structure
+        # The filename is not input filename, but name for processed data.
+        # The input is from `structures`.
+        # The filename extension does not matter, it will be replaced by `_data.pt`
+        filename="./data_for_prediction.txt",
+        r_cut=r_cut,
+        structures=structures,
+        **config,
     )
+
+    # remove the saved data, which is not needed for prediction
+    # (can be good for training)
+    shutil.rmtree(dataset.processed_dirname)
+
     return DataLoader(dataset=dataset, shuffle=False, batch_size=batch_size)
 
 
@@ -130,6 +169,7 @@ def predict(
     structures: Structure | list[Structure],
     model_path: Path = None,
     config_path: Path = None,
+    logger_level: str = "ERROR",
 ) -> ElasticTensor | list[ElasticTensor]:
     """
     Predict the property of a structure or a list of structures.
@@ -142,3 +182,23 @@ def predict(
     Returns:
         Predicted elastic tensor(s).
     """
+    set_logger("ERROR")
+
+
+if __name__ == "__main__":
+    set_logger("ERROR")
+
+    config = get_pretrained_config()
+
+    struct = Structure(
+        lattice=[
+            [3.348898, 0.0, 1.933487],
+            [1.116299, 3.157372, 1.933487],
+            [0.0, 0.0, 3.866975],
+        ],
+        species=["Si", "Si"],
+        coords=[[0.25, 0.25, 0.25], [0, 0, 0]],
+    )
+    structures = [struct, struct]
+
+    get_data_loader(structures, config, batch_size=2)
